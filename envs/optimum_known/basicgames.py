81737d31952b459642fac31603ebe72a18011574
Ben Black <weepingwillowben@gmail.com>
2020-03-23 18:50:49 -0400

    fixed basic game bugs

--- envs/optimum_known/basicgames.py
+++ envs/optimum_known/basicgames.py
@@ -44,14 +44,15 @@ class BlottoChoice:
 
 class CombChoice:
     def __init__(self,featurevec):
-        self.num_choices = num_choices
-        self.feature = self.random_features()
+        self.num_choices = len(featurevec)
+        self.feature = featurevec
     def random_choice(num_choices):
         return CombChoice(np.random.randint(0,1+1,size=num_choices))
     def random_alt(self):
         idx_flip = np.random.randint(0,self.num_choices)
         new_feature = np.copy(self.feature)
         new_feature[idx_flip] = 1-new_feature[idx_flip]
+        return CombChoice(new_feature)
     def __repr__(self):
         return str(self.feature)
     __str__ = __repr__
@@ -75,7 +76,7 @@ class RPC_CombChoice:
 class CombObjective:
     def __init__(self,num_combs):
         self.num_combs = num_combs
-        self.match_choice = CombChoice(self.num_combs)
+        self.match_choice = CombChoice.random_choice(num_combs)
     def sing_eval(self,choice):
         return np.sum(np.equal(self.match_choice.feature,choice.feature).astype(np.int32))
     def evaluate(self,choice1,choice2):
@@ -104,13 +105,13 @@ class RPCCombObjective:
         self.mul_val = mul_val
 
     def evaluate(self,choicep1,choicep2):
-        valp1 = self.comb_objectives[choicep1.rpc.feature].evaluate(choicep1.comb)
-        valp2 = self.comb_objectives[choicep1.rpc.feature].evaluate(choicep2.comb)
+        valp1 = self.comb_objectives[choicep1.rpc.feature].sing_eval(choicep1.comb)
+        valp2 = self.comb_objectives[choicep2.rpc.feature].sing_eval(choicep2.comb)
         rpc_val = self.rpc.evaluate(choicep1.rpc,choicep2.rpc)
         return valp1 - valp2 + rpc_val * self.mul_val
 
     def all_opt_choices(self):
-        return [RPC_CombChoice(self.comb_objectives[i],RPCChoice(i)) for i in range(3)]
+        return [RPC_CombChoice(self.comb_objectives[i].match_choice,RPCChoice(i)) for i in range(3)]
 
     def random_response(self):
         return RPC_CombChoice.random_choice(self.num_combs)
